87140b082529c046ab2fd6d0fe4c4a2bd9220497
Francisco Javier <fjplaurr@gmail.com>
2020-06-06 11:12:21 +0200

    include consts for redux

---
+++ frontend/src/utils/consts.js
@@ -0,0 +1,18 @@
+export const URIPARAMS = {
+  OCCASION: {
+    BIRTHDAY: 'birthday',
+    LOVE: 'love',
+    DECORATION: 'decoration',
+  },
+  COLOR: {
+    PINK: 'pink',
+    YELLOW: 'yellow',
+    RED: 'red',
+    ORANGE: 'orange',
+  },
+  PRICE: {
+    UNDER15: 'under15',
+    BETWEEN15AND40: 'between15and40',
+    OVER40: 'over40',
+  }
+}
