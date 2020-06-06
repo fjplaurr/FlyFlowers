90904a12b433b7719648f14727d11a47346166ef
Francisco Javier <fjplaurr@gmail.com>
2020-06-06 17:35:43 +0200

    add assets folder

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,17 @@
+import colorfulImg from './colorfulImg.jpg';
+import coverPage from './coverPage.jpg';
+import lightColorImg from './lightColorImg.jpg';
+import Calendar from './Calendar';
+import CalendarIcon from './CalendarIcon';
+import Flower from './Flower';
+import Rocket from './Rocket';
+
+export {
+  colorfulImg,
+  coverPage,
+  lightColorImg,
+  Calendar,
+  CalendarIcon,
+  Flower,
+  Rocket
+};
