cdb097cc960d233c27c7bb50476f1eb0df698b5a
Francisco Javier <fjplaurr@gmail.com>
2020-06-22 11:31:38 +0200

    serve index in build folder

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,13 @@ require('dotenv').config({ path: `${__dirname}/.env` });
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
 const flowersRoutes = require('./routes/flowersRoutes');
-const port = process.env.PORT || 5000;
+const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
+// Mongoose connection
 mongoose.connect(process.env.MLAB_URI, {
   keepAlive: true,
   useNewUrlParser: true,
@@ -17,6 +18,11 @@ mongoose.connect(process.env.MLAB_URI, {
 // Middlewares
 app.use(cors());
 app.use(express.json());
+const buildFolder = path.join(__dirname, '..', '/frontend', '/build');
+app.use(express.static(buildFolder));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(buildFolder, 'index.html'));
+});
 
 // Routers
 app.use('/api/flowers', flowersRoutes);
@@ -32,4 +38,4 @@ function errorHandler(error, request, response, next) {
 app.use(errorHandler);
 
 // Server listening
-app.listen(port);
+app.listen(PORT);
