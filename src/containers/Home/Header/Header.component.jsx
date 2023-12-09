5419ce95c326e6c89100c653a8a4102b47e98427
fjplaurr <fjplaurr@gmail.com>
2023-12-09 13:07:28 +0100

    refactor: simplify Home styles

--- src/containers/Home/Header/Header.component.jsx
+++ src/containers/Home/Header/Header.component.jsx
@@ -9,14 +9,11 @@ const Header = () => (
       src={coverPage}
       alt="Flowers that inspire"
     />
-    <div className={styles.captionHeader}>
-      <h1 className={styles.headerTitle}>
-        Something special
-        <br />
-        for someone unique
-      </h1>
-      <h2 className={styles.headerSubtitle}>Flowers that inspire</h2>
-    </div>
+    <h1 className={styles.headerTitle}>
+      Something special
+      <br />
+      for someone unique
+    </h1>
   </div>
 );
 
